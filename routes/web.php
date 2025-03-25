940ce06689bd65bc1f84a2aac3acf39732c72ded
ArAr <hhfrostie@gmail.com>
2025-03-25 11:19:35 +0800

    First commit: Done moving all of my project files

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Route::get('/login', 'App\Http\Controllers\LoginController@index');
+
+Route::get('/profile', 'App\Http\Controllers\ProfileController@index');
+
+Route::get('/hello', 'App\Http\Controllers\HelloController@index');
+
+Route::get('/create', 'App\Http\Controllers\CreateController@index');
