625c9ebc22020a5167bcae69bb88bb905d6ff05c
JOHNEL JOHN II GUANZON CA�AFUEGO <2121210@cc.usls.edu>
2025-03-27 11:23:09 +0800

    First commit: Done moving all of my project files

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -12,6 +12,7 @@
             </tr>
         </thead>
         <tbody>
+            
             @foreach($categories as $category)
                 <tr>
                     <td>{{ $category->id }}</td>
