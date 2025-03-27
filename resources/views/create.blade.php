862dbde5d6a6df38d1623482c0f92c5eff60ff6d
LINDSAY C. GAN <2200536@cc.usls.edu>
2025-03-27 10:58:13 +0800

    First commit: Done moving all of my project files

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -22,6 +22,7 @@
                     <option value="category1">Category 1</option>
                     <option value="category2">Category 2</option>
                     <option value="category3">Category 3</option>
+                    <option value="category3">Category 4</option>
                   </select>
                 </div>
 
